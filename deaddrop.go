3f3416c0912613de0c9cebbecf2794716906fcba
Alexsjones <tibbar@freedommail.ch>
2015-10-16 08:58:36 +0100

    allowing deletion afdter use

--- deaddrop.go
+++ deaddrop.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+  "bytes"
   _ "github.com/lib/pq"
   "flag"
+  "fmt"
   "os"
+  "io"
   "log"	
   "net/http"
   "encoding/json"
   "github.com/gorilla/mux"
   "github.com/AlexsJones/deaddrop/utils"
   "github.com/jinzhu/gorm"
+  "io/ioutil"
+  "html/template"
+  "strings"
 )
 
 var databaseConnect gorm.DB
@@ -21,6 +27,40 @@ var configuration utils.Configuration
 type incoming_data struct {
   Data string
   Guid string
+}
+
+func hdeaddrop_upload(w http.ResponseWriter, r *http.Request) {
+
+  file, header, err := r.FormFile("file") 
+  defer file.Close()
+
+  if err != nil {
+    fmt.Fprintln(w, err)
+    return
+  }
+
+  guid := utils.NewGuid()
+
+  hashedGuid := utils.Hash(guid)
+
+  filenameCipher := hashedGuid + "_" + header.Filename
+
+  out, err := os.Create("uploads/" + filenameCipher)
+
+  if err != nil {
+    fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+    return
+  }
+
+  defer out.Close()
+
+  _, err = io.Copy(out, file)
+  if err != nil {
+    fmt.Fprintln(w, err)
+  }
+
+  w.Write([]byte("Generating 1 time download link " + "http://" +
+  utils.GetHostnameIpv4() + ":" + port +  "/deaddrop/upload/" + hashedGuid))
 }
 
 func hdeaddrop_post(w http.ResponseWriter, r *http.Request) {
@@ -39,21 +79,43 @@ func hdeaddrop_post(w http.ResponseWriter, r *http.Request) {
 
   databaseConnect.Create(&i)
 
-  w.Write([]byte("Generating 1 time download link " + "http://" +
-  utils.GetHostnameIpv4() + ":" + port +  "/deaddrop/" + i.Guid))
 }
 
 func hdeaddrop_get(w http.ResponseWriter, r *http.Request) {
   params := mux.Vars(r)
   id := params["id"]
 
-  var result incoming_data
+  dirname := "uploads"
+  d, err := os.Open(dirname)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  defer d.Close()
+  fi, err := d.Readdir(-1)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  for _, fi := range fi {
+    if fi.Mode().IsRegular() {
 
-  databaseConnect.Where(&incoming_data{ Guid: id}).First(&result)
+      splitString := strings.Split(fi.Name(),"_") 
 
-  databaseConnect.Where(&incoming_data{ Guid: id}).Delete(&incoming_data{})
+      if splitString[0] == id {
 
-  w.Write([]byte(result.Data))
+	file := "uploads/" + fi.Name()
+	
+	log.Println(file)
+
+	http.ServeFile(w,r,file)
+
+	/* Delete file */
+	os.Remove(file)
+      }
+    }
+  }
+  w.Write([]byte("404"))
 }
 
 func initialiseDatabase() {
@@ -80,6 +142,23 @@ func initialiseDatabase() {
 
 }
 
+func hdeaddrop_home(w http.ResponseWriter, r *http.Request) {
+
+  s1, _ := template.ParseFiles("tmpl/header.tmpl", 
+  "tmpl/content.tmpl", "tmpl/footer.tmpl")
+
+  fbody, err := ioutil.ReadFile("views/index.html")
+  if err != nil {
+
+  }
+  var buffer bytes.Buffer
+  s1.ExecuteTemplate(&buffer, "header", nil)
+  s1.ExecuteTemplate(&buffer, "content", template.HTML(string(fbody)))
+  s1.ExecuteTemplate(&buffer, "footer", nil)
+
+  w.Write(buffer.Bytes())
+}
+
 func main() {
 
   if os.Getenv("DEADDROP_CONF")  != "" {
@@ -100,9 +179,11 @@ func main() {
 
   rtr := mux.NewRouter()
 
-  rtr.HandleFunc("/deaddrop/{id}", hdeaddrop_get).Methods("GET")
+  rtr.HandleFunc("/deaddrop/upload/{id}", hdeaddrop_get).Methods("GET")
 
-  rtr.HandleFunc("/deaddrop", hdeaddrop_post).Methods("POST")
+  rtr.HandleFunc("/deaddrop/upload",hdeaddrop_upload).Methods("POST")
+
+  rtr.HandleFunc("/",hdeaddrop_home).Methods("GET")
 
   http.Handle("/",rtr)
 
